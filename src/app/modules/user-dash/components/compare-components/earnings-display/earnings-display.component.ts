c777cc367e6b5c6f741a8d6170c357dbd5878fa1
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-10-24 12:47:51 -0500

    #34 earnings added

--- src/app/modules/user-dash/components/compare-components/earnings-display/earnings-display.component.ts
+++ src/app/modules/user-dash/components/compare-components/earnings-display/earnings-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { IEstimate } from '../../../shared/interfaces/IEstimate';
 import { EstimateData } from '../../../shared/models/EstimateData';
 import { ListServiceService } from '../../../shared/services/list-service.service';
@@ -17,33 +17,51 @@ export class EarningsDisplayComponent implements OnInit {
 
   estimates:IEstimate[] = [];
   estimatesDisplay:EstimateData;
+  isError:boolean = false;
+  errMessage:string = '';
   ngOnInit(): void {
-    this.getEarnings();
   }
   //add a loading icon when finished
   getEarnings(){
+    if (this.rightSymbol === undefined || this.leftSymbol === undefined) return;
+    this.estimates = [];
     this.estimatesDisplay = new EstimateData();
     console.log(this.leftSymbol, this.rightSymbol);
-    let el = "JPM";
-    let ei = "ROKU";
-    this._stocks.getNextEarningsReport("JPM,ROKU")
+    this._stocks.getNextEarningsReport(`${this.leftSymbol},${this.rightSymbol}`)
     .subscribe
     (data => {
+      console.log(data)
       for (const [key, value] of Object.entries(data)){
         this.estimates.push(data[key]);
+        if(Object.keys(data[key].estimates).length === 0 && data[key].estimates.constructor === Object){
+          this.isError = true;
+          this.errMessage = "Object was empty";
+          break;
+        }
+        else this.isError = false;
         console.log("Estimates", this.estimates)
       }
-      this.estimatesDisplay.createNewSets(this.estimates);
+      console.log(this.isError)
+      if(this.isError !== true) this.estimatesDisplay.createNewSets(this.estimates);
       console.log(this.estimatesDisplay);
     },
-    error => console.log(error),
+    error => {
+      this.isError = true;
+      this.errMessage = error;
+    },
     () => console.log("Done with earnings")
     );
   }
 
   //on changes call getEarnings again
-  ngOnChanges(){
-
+  ngOnChanges(changes:SimpleChanges){
+    console.log("Earnings changes: ", changes);
+    if (changes.left === undefined || changes.right === undefined) console.log("hi");
+    else if (changes.left.isFirstChange() && changes.right.isFirstChange()) return;
+    if (changes.left !== undefined) this.leftSymbol = changes.left.currentValue;
+    if (changes.right !== undefined) this.rightSymbol = changes.right.currentValue;
+   // if (changes.left !== undefined && changes.right !== undefined) this.getEarnings();
+   this.getEarnings()
   }
 
 }
